265be340a7baf268ae8d3ea7746c0ec33a96ce75
Jonathan Earl <jonathan.earl@tylertech.com>
2022-02-08 14:44:21 -0800

    use promises , add vars for logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { catchError, tap } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,55 +10,53 @@ import { catchError, tap } from 'rxjs';
 export class AppComponent {
   title = 'test-s3-presigned-upload';
   customerId = 'tide-broker'
-  presignedLogoEndpoint = `http://localhost:5010/api/v1/LogoUpload/customer/${this.customerId}/presignedUrl`;
-  logoFileName = 'logo';
+
+  // controller = 'BannerUpload';
+  // fileType = 'banner';
+
+  controller = 'LogoUpload';
+  fileType = 'logo';
+
+  api = `http://localhost:5010/api/v1/${this.controller}`;
   url: string | undefined
   file: File | undefined;
 
   constructor(private _http: HttpClient) {
-    this.presignedLogoEndpoint
   }
 
-  handleChange(event: any) {
+  async handleChange(event: any) {
     const file: File = event.target?.files?.[0];
     this.file = file;
     console.log(`file: ${file.name} has been selected`);
-    this.getPresignedUrl(file.type);
+    await this.getPresignedUrl(file.type);
   }
 
-  getPresignedUrl(fileType: string) {
+  async getPresignedUrl(fileType: string) {
+    console.log('retrieveing presigned url');
     const extension = fileType.split("/").pop();
     const options = {
       params: { extension: `.${extension}` },
       responseType: 'text' as 'json'
     }
-    this._http.get<string>(this.presignedLogoEndpoint, options)
-      .pipe(tap(url => this.url = url))
-      .pipe(tap(url => console.log(`retrieved url: ${url} from endpoint ${this.presignedLogoEndpoint}`)))
-      .pipe(catchError(err => {
-        console.log(err.error)
-        return err;
-      }))
-      .subscribe()
+    const result = await firstValueFrom(this._http.get<string>(`${this.api}/customer/${this.customerId}/presignedUrl`, options));
+    console.log({ result });
+    this.url = result;
   }
 
-  uploadImage() {
+  async uploadImage() {
     console.log('uploading image...', this.url, this.file)
-    if(!this.url) { console.log('presigned url missing'); return; }
-    if(!this.file) { console.log('file missing'); return; }
+    if (!this.url) { console.log('presigned url missing'); return; }
+    if (!this.file) { console.log('file missing'); return; }
 
     // pull extension off file name
     const extension = this.file.type.split("/").pop();
 
     // not sure why this is required, but will fail 403 without renaming the file
-    const myFile = new File([this.file], `${this.logoFileName}.${extension}`);
+    const myFile = new File([this.file], `${this.fileType}.${extension}`);
 
-    this._http.put(this.url, myFile)
-      .pipe(tap(res => console.log(res)))
-      .pipe(catchError(err => {
-        console.log(JSON.stringify(err))
-        return err;
-      }))
-      .subscribe();
+    await firstValueFrom(this._http.put(this.url, myFile));
+    await firstValueFrom(
+      this._http.post<string>(`${this.api}/customer/${this.customerId}/publishStaged`, null)
+    )
   }
 }
